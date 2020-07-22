ce9d228389e1939df5a9346a98cba7dfd59acca8
Umberto <umbertopellegrino96@gmail.com>
2020-07-22 15:28:56 +0200

    writeOnHtml()

--- js/jscript.js
+++ js/jscript.js
@@ -1,5 +1,39 @@
-function init() {
+function downloadAlbum() {
+  var album = [];
+  $.ajax({
+    url:'https://flynn.boolean.careers/exercises/api/array/music',
+    method:'GET',
+    success:function (data) {
+      album = data['response'];
+      writeOnHtml(album);
+    },
+    error:function (error) {
+      console.log('error');
+    }
+  });
 
+}
+
+function writeOnHtml(album) {
+  var template = $('#cd-template').html();
+  var compiled = Handlebars.compile(template)
+  var target = $('#album');
+
+  for (var i = 0; i < album.length; i++) {
+    var cd = album[i];
+    var cdHTML= compiled({
+      'poster': cd['poster'],
+      'title' : cd['title'],
+      'author': cd['author'],
+      'genre': cd['genre'],
+      'year': cd['year']
+    });
+    target.append(cdHTML);
+  }
+}
+
+function init() {
+  downloadAlbum()
 }
 
 $(document).ready(init);
