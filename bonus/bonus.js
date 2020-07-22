0ded82c3473a79bfc908d5fa3fea5eb4d845fa9a
Umberto <umbertopellegrino96@gmail.com>
2020-07-22 16:01:42 +0200

    Bonus choice tips

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,4 +1,10 @@
-function downloadAlbum() {
+
+function bonustips() {
+  var btn = $('#btnAlbum');
+
+  btn.click(function () {
+    $('#album').html('');
+    var choice = $('.genreTips').val();
 
     var album = [];
     $.ajax({
@@ -6,35 +12,39 @@ function downloadAlbum() {
       method:'GET',
       success:function (data) {
         album = data['response'];
-        writeOnHtml(album);
+        choiceOnHtml(album , choice);
       },
       error:function (error) {
         console.log('error');
       }
     });
+  });
 
 }
 
-function writeOnHtml(album) {
+function choiceOnHtml(album , choice) {
   var template = $('#cd-template').html();
   var compiled = Handlebars.compile(template)
   var target = $('#album');
 
   for (var i = 0; i < album.length; i++) {
     var cd = album[i];
-    var cdHTML= compiled({
-      'poster': cd['poster'],
-      'title' : cd['title'],
-      'author': cd['author'],
-      'genre': cd['genre'],
-      'year': cd['year']
-    });
-    target.append(cdHTML);
+    if (cd['genre'] == choice) {
+      var cdHTML= compiled({
+        'poster': cd['poster'],
+        'title' : cd['title'],
+        'author': cd['author'],
+        'genre': cd['genre'],
+        'year': cd['year']
+      });
+      target.append(cdHTML);
+    }
+
   }
 }
 
 function init() {
-  downloadAlbum()
+  bonustips();
 }
 
 $(document).ready(init);
